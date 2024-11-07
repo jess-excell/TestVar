eedf5a530bb61b65f6d33e751abf3bdeeae6306b
jess-excell <jessexcell@outlook.com>
2024-11-07 14:20:18 +0000

    Add project and initial apps

---
+++ flashcard/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from enum import Enum
+
+# Create your models here.
+# class Difficulty(Enum):
+#     EASY = "easy"
+#     MEDIUM = "medium"
+#     HARD = "hard"
+
+# class FlashCard(models.Model):
+#     question = models.TextField()
+#     answer = models.TextField()
+#     difficulty = models.TextField(
+#         choices=[(tag.value, tag.name.title()) for tag in Difficulty]
+#     )
+
+# class FlashcardSet(models.Model):
+#     name = models.TextField()
+#     cards = models.ManyToOneField(FlashCard, related_name="sets")
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+
+
+# class Comment(models.Model):
+#     comment = models.TextField()
+#     set = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, related_name="comments")
+#     author = models.User()
