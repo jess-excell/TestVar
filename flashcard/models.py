3475381539f1f74d0333dc4d31aa0de270d59383
jess-excell <jessexcell@outlook.com>
2024-11-11 16:49:11 +0000

    UI formatting changes

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from enum import Enum
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Difficulty(Enum):
@@ -13,12 +14,14 @@ class FlashCard(models.Model):
     difficulty = models.TextField(
         choices=[(tag.value, tag.name.title()) for tag in Difficulty]
     )
+#   flashcardSet = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE)
 
 # class FlashcardSet(models.Model):
 #     name = models.TextField()
 #     cards = models.ManyToOneField(FlashCard, related_name="sets")
 #     created_at = models.DateTimeField(auto_now_add=True)
 #     updated_at = models.DateTimeField(auto_now_add=True)
+#     user = models.ForeignKey(User)
 
 # class Comment(models.Model):
 #     comment = models.TextField()
