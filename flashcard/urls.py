65bca9e2e1ddbd6b7b8bbb92101dcc9fdb5e58f4
jess-excell <jessexcell@outlook.com>
2024-11-20 13:34:55 +0000

    Add ability to create, update and delete flashcard, flashcard sets and flashcard set collections. (may have bugs)

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -6,13 +6,19 @@ urlpatterns = [
     # Collections
     path('collections', FlashcardCollectionListView.as_view(), name="collection-list"),
     path('collections/create', FlashcardCollectionCreateView.as_view(), name="collection-create"),
+    path('collections/<int:collection_id>/delete', FlashcardCollectionDeleteView.as_view(), name="collection-delete"),
+    path('collections/<int:collection_id>/update', FlashcardCollectionUpdateView.as_view(), name="collection-update"),
     
     # Sets
     path("collections/<int:collection_id>", FlashcardSetListView.as_view(), name="set-list"),
     path('collections/<int:collection_id>/create', views.FlashcardSetCreateView.as_view(), name="set-create"),
+    path('collections/<int:collection_id>/<int:set_id>/delete', FlashcardSetDeleteView.as_view(), name="set-delete"),
+    path('collections/<int:collection_id>/<int:set_id>/update', FlashcardSetUpdateView.as_view(), name="set-update"),
     
     # Flashcards
     path('collections/<int:collection_id>/<int:set_id>', FlashcardListView.as_view(), name="flashcard-list"),
     path('collections/<int:collection_id>/<int:set_id>/<int:flashcard_id>', FlashcardDetailView.as_view(), name="flashcard-detail"),
     path('collections/<int:collection_id>/<int:set_id>/create', views.FlashcardCreateView.as_view(), name="flashcard-create"),
+    path('collections/<int:collection_id>/<int:set_id>/<int:flashcard_id>/delete', FlashCardDeleteView.as_view(), name="flashcard-delete"),
+    path('collections/<int:collection_id>/<int:set_id>/<int:flashcard_id>/update', FlashCardUpdateView.as_view(), name="flashcard-update"),
 ]
--- flashcard/views.py
+++ flashcard/views.py
@@ -5,8 +5,81 @@ from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from .models import FlashCard, FlashcardSet, FlashcardCollection
+from django.urls import reverse
+import datetime
 
-# Collection views
+#region CreateViews (Create)
+class FlashcardCollectionCreateView(LoginRequiredMixin, CreateView):
+    model=FlashcardCollection
+    fields=['title', 'description', 'public']
+    template_name="flashcard/flashcard_collection_create.html"
+    login_url="/login"
+    
+    # Add user to collection info
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def get_success_url(self):
+        return reverse("collection-list")
+    
+class FlashcardSetCreateView(LoginRequiredMixin, CreateView):
+    model = FlashcardSet
+    fields = ['title', 'description']
+    template_name="flashcard/flashcard_set_create.html"
+    login_url="/login"
+
+    # Get just flashcard collection id
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['collection_id'] = self.kwargs['collection_id']
+        return context
+    
+    def form_valid(self, form):
+        collection = FlashcardCollection.objects.get(pk=self.kwargs["collection_id"])
+        form.instance.flashcard_collection = collection
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("set-list", kwargs={
+            "collection_id": self.kwargs.get('collection_id'),
+        })
+    
+class FlashcardCreateView(LoginRequiredMixin, CreateView):
+    model = FlashCard
+    fields = ['question', 'answer', 'difficulty']
+    template_name="flashcard/flashcard_create.html"
+    login_url="/login"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['collection_id'] = self.kwargs.get('collection_id')
+        context['set_id'] = self.kwargs.get('set_id')
+        collection = get_object_or_404(FlashcardCollection, id=context['collection_id'])
+
+        if not collection.public and collection.user != self.request.user:
+            raise Http404("You do not have permission to edit this set.")
+        
+        return context
+    
+    # Add user to collection info
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.flashcard_set = get_object_or_404(FlashcardSet, id=self.kwargs.get('set_id'))
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def get_success_url(self):
+        return reverse("flashcard-list", kwargs={
+            "collection_id": self.kwargs.get('collection_id'),
+            "set_id": self.kwargs.get("set_id"),
+        })
+# endregion
+
+# region ListViews and FlashcardDetailView (Read)
 class FlashcardCollectionListView(ListView):
     model = FlashcardCollection
     context_object_name = "collections"
@@ -18,27 +91,7 @@ class FlashcardCollectionListView(ListView):
             return FlashcardCollection.objects.filter(Q(public=True) | Q(user=self.request.user))
         else:
             return FlashcardCollection.objects.filter(public=True)
-
-class FlashcardCollectionUpdateView(UpdateView):
-    model = FlashcardCollection
-    fields = ['title', 'description']
-    template_name = "flashcard/flashcard_collection_update.html"
-
-class FlashcardCollectionCreateView(LoginRequiredMixin, CreateView):
-    model=FlashcardCollection
-    fields=['title', 'description', 'public']
-    template_name="flashcard/flashcard_collection_create.html"
-    login_url="/login"
-    success_url='/'
-    
-    # Add user to collection info
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.user = self.request.user
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
-
-# Set views
+        
 class FlashcardSetListView(ListView):
     model = FlashcardSet
     context_object_name = "sets"
@@ -57,26 +110,7 @@ class FlashcardSetListView(ListView):
             raise Http404("You do not have permission to view this collection.")
 
         return context
-
-class FlashcardSetCreateView(LoginRequiredMixin, CreateView):
-    model = FlashcardSet
-    fields = ['title', 'description']
-    template_name="flashcard/flashcard_set_create.html"
-    login_url="/login"
-    success_url="/"
-
-    # Get just flashcard collection id
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['collection_id'] = self.kwargs['collection_id']
-        return context
     
-    def form_valid(self, form):
-        collection = FlashcardCollection.objects.get(pk=self.kwargs["collection_id"])
-        form.instance.flashcard_collection = collection
-        return super().form_valid(form)
-
-# Flashcard views
 class FlashcardListView(ListView):
     model = FlashCard
     context_object_name = "flashcards"
@@ -94,14 +128,14 @@ class FlashcardListView(ListView):
         
         context['collection_id'] = collection_id
         context['set_id'] = set_id
-        collection = get_object_or_404(FlashcardCollection, id=collection_id)
+        context["flashcard_collection"] = get_object_or_404(FlashcardCollection, id=collection_id)
         
-        if collection.user != self.request.user and not collection.public:
+        if context["flashcard_collection"].user != self.request.user and not context["flashcard_collection"].public:
             raise Http404("You do not have permission to view this collection.")
         
         context['flashcard_set'] = get_object_or_404(FlashcardSet, id=set_id)
         return context
-
+    
 class FlashcardDetailView(DetailView):
     model = FlashCard
     context_object_name = "flashcard"
@@ -112,40 +146,173 @@ class FlashcardDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['collection_id'] = self.kwargs.get('collection_id')
         context['set_id'] = self.kwargs.get('set_id')
-        
-        collection_id = self.kwargs.get('collection_id')
-        collection = get_object_or_404(FlashcardCollection, id=collection_id)
+        collection = get_object_or_404(FlashcardCollection, id=context['collection_id'])
 
         if not collection.public and collection.user != self.request.user:
             raise Http404("You do not have permission to view this collection.")
         
         return context
+# endregion
 
+# region UpdateViews (Update)
+class FlashcardCollectionUpdateView(LoginRequiredMixin, UpdateView):
+    model = FlashcardCollection
+    template_name = "flashcard/flashcard_collection_update.html"
+    pk_url_kwarg = "collection_id"
+    login_url = "/login"
+    fields = ["title", "description", "public"]
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        collection_id = self.kwargs.get('collection_id')
+        
+        context['flashcard_collection'] = get_object_or_404(FlashcardCollection, id=collection_id)
+        if context['flashcard_collection'].user != self.request.user:
+            raise Http404("You do not have permission to modify this collection.")
+        return context
+    
+    def get_success_url(self):
+        return reverse("set-list", kwargs={
+            "collection_id": self.kwargs.get('collection_id'),
+        })
+    
+class FlashcardSetUpdateView(LoginRequiredMixin, UpdateView):
+    model = FlashcardSet
+    template_name = "flashcard/flashcard_set_update.html"
+    pk_url_kwarg = "set_id"
+    login_url = "/login"
+    fields = ["title", "description"]
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        collection_id = self.kwargs.get('collection_id')
+        set_id = self.kwargs.get('set_id')
+        
+        context['collection_id'] = collection_id
+        context['set_id'] = set_id
+        collection = get_object_or_404(FlashcardCollection, id=collection_id)
+        
+        if collection.user != self.request.user:
+            raise Http404("You do not have permission to modify this set.")
+        
+        context['flashcard_set'] = get_object_or_404(FlashcardSet, id=set_id)
+        context['flashcard_collection'] = get_object_or_404(FlashcardCollection, id=collection_id)
+        return context    
+    
+    # Add updated date to set info
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.updated_at = datetime.datetime.now()
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def get_success_url(self):
+        return reverse("flashcard-list", kwargs={
+            "collection_id": self.kwargs.get('collection_id'),
+            "set_id": self.kwargs.get("set_id"),
+        })
 
-class FlashcardCreateView(LoginRequiredMixin, CreateView):
+class FlashCardUpdateView(LoginRequiredMixin, UpdateView):
     model = FlashCard
-    fields = ['question', 'answer', 'difficulty']
-    template_name="flashcard/flashcard_create.html"
-    success_url = '/'
-    login_url="/login"
+    template_name = "flashcard/flashcard_update.html"
+    pk_url_kwarg = "flashcard_id"
+    login_url = "/login"
+    fields = ["question", "answer", "difficulty"]
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        collection_id = self.kwargs.get('collection_id')
+        set_id = self.kwargs.get('set_id')
+        flashcard_id = self.kwargs.get('flashcard_id')
+        
+        context['collection_id'] = collection_id
+        context['set_id'] = set_id
+        context['flashcard_id'] = flashcard_id
+        collection = get_object_or_404(FlashcardCollection, id=collection_id)
+        
+        if collection.user != self.request.user:
+            raise Http404("You do not have permission to modify this set.")
+        
+        context['flashcard_collection'] = get_object_or_404(FlashcardCollection, id=collection_id)
+        context['flashcard'] = get_object_or_404(FlashCard, id=flashcard_id)
+        return context    
+    
+    def get_success_url(self):
+        return reverse("flashcard-detail", kwargs={
+            "collection_id": self.kwargs.get('collection_id'),
+            "set_id": self.kwargs.get("set_id"),
+            "flashcard_id": self.kwargs.get("flashcard_id")
+        })
+# endregion
+
+# region DeleteViews (Delete)
+class FlashcardCollectionDeleteView(LoginRequiredMixin, DeleteView):
+    model = FlashcardCollection
+    template_name = "flashcard/flashcard_collection_delete.html"
+    pk_url_kwarg = "collection_id"
+    login_url = "/login"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        collection_id = self.kwargs.get('collection_id')
+        
+        context['flashcard_collection'] = get_object_or_404(FlashcardCollection, id=collection_id)
+        if context['flashcard_collection'].user != self.request.user:
+            raise Http404("You do not have permission to modify this collection.")
+
+        return context
+    
+    def get_success_url(self):
+        return reverse("collection-list")
+
+class FlashcardSetDeleteView(LoginRequiredMixin, DeleteView):
+    model = FlashcardSet
+    template_name = "flashcard/flashcard_set_delete.html"
+    pk_url_kwarg = "set_id"
+    login_url = "/login"
+    
+    # Get flashcard collection info
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        collection_id = self.kwargs.get('collection_id')
+        set_id = self.kwargs.get('set_id')
+        
+        context['collection_id'] = collection_id
+        context['set_id'] = set_id
+        collection = get_object_or_404(FlashcardCollection, id=collection_id)
+        
+        if collection.user != self.request.user and not collection.public:
+            raise Http404("You do not have permission to modify this set.")
+        
+        context['flashcard_collection'] = get_object_or_404(FlashcardCollection, id=collection_id)
+        context['flashcard_set'] = get_object_or_404(FlashcardSet, id=set_id)
+        return context
+    
+    def get_success_url(self):
+        return reverse("set-list", kwargs={
+            "collection_id": self.kwargs.get('collection_id'),
+        })
+    
+class FlashCardDeleteView(LoginRequiredMixin, DeleteView):
+    model = FlashCard
+    template_name = "flashcard/flashcard_delete.html"
+    pk_url_kwarg="flashcard_id"
+    login_url = "/login"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['collection_id'] = self.kwargs.get('collection_id')
         context['set_id'] = self.kwargs.get('set_id')
-        
-        collection_id = self.kwargs.get('collection_id')
-        collection = get_object_or_404(FlashcardCollection, id=collection_id)
+        context["flashcard_collection"] = get_object_or_404(FlashcardCollection, id=context['collection_id'])
 
-        if not collection.public and collection.user != self.request.user:
+        if context["flashcard_collection"].user != self.request.user:
             raise Http404("You do not have permission to edit this set.")
         
         return context
     
-    # Add user to collection info
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.user = self.request.user
-        self.object.flashcard_set = get_object_or_404(FlashcardSet, id=self.kwargs.get('set_id'))
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
+    def get_success_url(self):
+        return reverse("flashcard-list", kwargs={
+            "collection_id": self.kwargs.get('collection_id'),
+            "set_id": self.kwargs.get("set_id")
+        })
+# endregion
