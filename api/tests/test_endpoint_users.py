5c4957e1653aa32d8ccd688a92076de8e6af4b62
jess-excell <jessexcell@outlook.com>
2024-11-30 15:54:35 +0000

    Formatting changes to test_endpoint_users to make tests more readable. Also fix some incorrect assertEqual checks and other errors in tests. + All tests are (currently) passing

--- api/tests/test_endpoint_users.py
+++ api/tests/test_endpoint_users.py
@@ -2,51 +2,52 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
 
-class SetUp():
-    def create_and_login_as_superuser(self):
-        self.user = User.objects.create_superuser(username='testuser', password='testpassword')
-        self.client.login(username='testuser', password='testpassword')
-    def create_and_login_as_standard_user(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.login(username='testuser', password='testpassword')
+class EndpointTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.superuser = User.objects.create_superuser(
+            username="superuser", 
+            password="superpassword")
+        cls.standard_user = User.objects.create_user(
+            username="standard_user", 
+            password="userpassword")
 
-class EndpointAuthTestGetAll(APITestCase):  
-    def test_get_users_logged_out(self):
+    def setUp(self):
+        self.client.logout()
+    
+    def test_get_users_as_logged_out_user(self):
         response = self.client.get('/api/users/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
     
-    def test_get_users_invalid_privilege(self):
+    def test_get_users_as_standard_user(self):
         # Create low level user
-        SetUp.create_and_login_as_standard_user(self)
+        self.client.login(username="standard_user", password="userpassword")
         response = self.client.get('/api/users/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
     
-    def test_get_users_valid_privilege(self):
-        SetUp.create_and_login_as_superuser(self)
+    def test_get_users_as_superuser(self):
+        self.client.login(username="superuser", password="superpassword")
         response = self.client.get('/api/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-class EndpointAuthTestCreate(APITestCase):        
-    def test_create_user_max_privilege(self):
-        SetUp.create_and_login_as_superuser(self)
+    def test_create_user_as_superuser(self):
+        self.client.login(username="superuser", password="superpassword")
         response = self.client.post(path='/api/users/', data={
             "username": "test",
             "password": "9EMxLB]632p",
             "is_staff": False
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
-    # FAILING
+    
     def test_create_user_as_standard_user(self):
-        SetUp.create_and_login_as_standard_user(self)
+        self.client.login(username="standard_user", password="userpassword")
         response = self.client.post(path='/api/users/', data={
             "username": "test",
             "password": "9EMxLB]632p",
             "is_staff": False
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
-    # FAILING
+    
     def test_create_user_logged_out(self):
         response = self.client.post(path='/api/users/', data={
             "username": "test",
@@ -55,19 +56,17 @@ class EndpointAuthTestCreate(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
-class EndpointBadRequestTestCreate(APITestCase):
     def test_create_user_good_request(self):
-        SetUp.create_and_login_as_superuser(self)
+        self.client.login(username="superuser", password="superpassword")
         response = self.client.post(path='/api/users/', data={
             "username": "test",
             "password": "9EMxLB]632p",
             "is_staff": False
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
-    # FAILING
+    
     def test_create_user_empty_password(self):
-        SetUp.create_and_login_as_superuser(self)
+        self.client.login(username="superuser", password="superpassword")
         response = self.client.post(path='/api/users/', data={
             "username": "test",
             "password": "",
@@ -75,9 +74,8 @@ class EndpointBadRequestTestCreate(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
-    # FAILING
     def test_create_user_empty_username(self):
-        SetUp.create_and_login_as_superuser(self)
+        self.client.login(username="superuser", password="superpassword")
         response = self.client.post(path='/api/users/', data={
             "username": "",
             "password": "9EMxLB]632p",
@@ -85,30 +83,24 @@ class EndpointBadRequestTestCreate(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
     
-class EndpointAuthTestDelete(APITestCase):
-    def test_delete_low_level_user(self):
-        # Create low level user
-        SetUp.create_and_login_as_superuser(self)
+    def test_delete_standard_user(self):
+        self.client.login(username="superuser", password="superpassword")
         second_user = User.objects.create_user(username='testuser2', password='testpassword')
         response = self.client.delete(f'/api/users/{second_user.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         
-    # FAILING
     def test_delete_superuser(self):
-        SetUp.create_and_login_as_superuser(self)
+        self.client.login(username="superuser", password="superpassword")
         second_user = User.objects.create_superuser(username='testuser2', password='testpassword')
         response = self.client.delete(f'/api/users/{second_user.id}/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
     
-    # FAILING
     def test_delete_self(self):
-        # Create low level user
-        SetUp.create_and_login_as_standard_user(self)
-        response = self.client.delete(f'/api/users/{self.user.id}/')
+        self.client.login(username="standard_user", password="userpassword")
+        response = self.client.delete(f'/api/users/{self.standard_user.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)        
         
-class EndpointAuthTestUpdate(APITestCase):
-    def test_update_user_logged_out(self):
+    def test_put_user_logged_out(self):
         second_user = User.objects.create_user(username='testuser2', password='testpassword')
         response = self.client.put(path=f'/api/users/{second_user.id}/', data={
             "username": "test2",
@@ -117,8 +109,8 @@ class EndpointAuthTestUpdate(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         
-    def test_update_user_invalid_privilege(self):
-        SetUp.create_and_login_as_standard_user(self)
+    def test_put_user_as_standard_user(self):
+        self.client.login(username="standard_user", password="userpassword")
         second_user = User.objects.create_user(username='testuser2', password='testpassword')
         response = self.client.put(path=f'/api/users/{second_user.id}/', data={
             "username": "test2",
@@ -127,8 +119,8 @@ class EndpointAuthTestUpdate(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         
-    def test_update_user_valid_privilege(self):
-        SetUp.create_and_login_as_superuser(self)
+    def test_put_user_as_superuser(self):
+        self.client.login(username="superuser", password="superpassword")
         second_user = User.objects.create_user(username='testuser2', password='testpassword')
         response = self.client.put(path=f'/api/users/{second_user.id}/', data={
             "username": "test2",
@@ -137,48 +129,49 @@ class EndpointAuthTestUpdate(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-class EndpointBadRequestTestUpdate(APITestCase):
-    def test_update_user_good_request(self):
-        SetUp.create_and_login_as_superuser(self)
-        response = self.client.post(path='/api/users/', data={
+    def test_put_user_good_request(self):
+        self.client.login(username="superuser", password="superpassword")
+        second_user = User.objects.create_user(username='testuser2', password='testpassword')
+        response = self.client.put(path=f'/api/users/{second_user.id}/', data={
             "username": "test",
             "password": "",
             "is_staff": False
         })
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-    def test_update_user_empty_password(self):
-        SetUp.create_and_login_as_superuser(self)
-        response = self.client.post(path='/api/users/', data={
+    def test_put_user_empty_password(self):
+        self.client.login(username="superuser", password="superpassword")
+        second_user = User.objects.create_user(username='testuser2', password='testpassword')
+        response = self.client.put(path=f'/api/users/{second_user.id}/', data={
             "username": "test",
             "password": "",
             "is_staff": False
         })
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-    def test_update_user_empty_username(self):
-        SetUp.create_and_login_as_superuser(self)
-        response = self.client.post(path='/api/users/', data={
+    def test_put_user_empty_username(self):
+        self.client.login(username="superuser", password="superpassword")
+        second_user = User.objects.create_user(username='testuser2', password='testpassword')
+        response = self.client.put(path=f'/api/users/{second_user.id}/', data={
             "username": "",
             "password": "9EMxLB]632p",
             "is_staff": False
         })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
     
-class EndpointAuthTestGetById(APITestCase):
     def test_get_user_by_id_logged_out(self):
         second_user = User.objects.create_user(username='testuser2', password='testpassword')
         response = self.client.get(path=f'/api/users/{second_user.id}/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         
-    def test_get_user_by_id_invalid_privilege(self):
-        SetUp.create_and_login_as_standard_user(self)
+    def test_get_user_by_id_as_standard_user(self):
+        self.client.login(username="standard_user", password="userpassword")
         second_user = User.objects.create_user(username='testuser2', password='testpassword')
         response = self.client.get(path=f'/api/users/{second_user.id}/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         
-    def test_get_user_by_id_valid_privilege(self):
-        SetUp.create_and_login_as_superuser(self)
+    def test_get_user_by_id_as_superuser(self):
+        self.client.login(username="superuser", password="superpassword")
         second_user = User.objects.create_user(username='testuser2', password='testpassword')
         response = self.client.get(path=f'/api/users/{second_user.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
