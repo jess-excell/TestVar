fac825232e9941d6bff1229832bcd7ebf91d2b22
jess-excell <jessexcell@outlook.com>
2024-12-03 16:40:12 +0000

    Add home tests and small fix to login.html

--- home/tests/test_templates.py
+++ home/tests/test_templates.py
@@ -1,23 +1,51 @@
 from django.test import TestCase
 from django.urls import reverse
 
-class TemplateTests(TestCase):
-    def test_homepage_template(self):
-        response = self.client.get('')
-        self.assertTemplateUsed(response, 'home/hello-world.html')
-        self.assertContains(response, 'TestVar')
+class HomePageTemplateTests(TestCase):
+    def test_template_used(self):
+        response = self.client.get("")
+        self.assertTemplateUsed(response, "home/hello-world.html")
+    
+    # Checks that the title, subheading, main content and footer are all there
+    def test_template_content(self):
+        response = self.client.get("")
+        self.assertContains(response, "TestVar")
+        self.assertContains(response, "Studying - made simple.")
+        self.assertContains(response, "TestVar presents a state of the art flashcard learning experience, featuring the ability to share and collaborate with fellow students.")
+        self.assertContains(response, "© TestVar (2024). All rights reserved.")
+
+class LoginPageTemplateTests(TestCase):
+    def test_template_used(self):
+        response = self.client.get("/login")
+        self.assertTemplateUsed(response, "home/login.html")
         
-    def test_login_template(self):
-        response = self.client.get('/login')
-        self.assertTemplateUsed(response, 'home/login.html')
-        self.assertContains(response, 'Log in')
-        
-    def test_logout_template(self):
-        response = self.client.get('/logout')
-        self.assertTemplateUsed(response, 'home/logout.html')
+    def test_template_content(self):
+        response = self.client.get("/login")
+        self.assertContains(response, "Log in")
+        self.assertContains(response, "Username:")
+        self.assertContains(response, "Password:")
+        self.assertContains(response, "Submit")
+
+class LogoutPageTemplateTests(TestCase):
+    def test_template_used(self):
+        response = self.client.get("/logout")
+        self.assertTemplateUsed(response, "home/logout.html")
+    
+    def test_template_content(self):
+        response = self.client.get("/logout")
         self.assertContains(response, "You've been logged out")
-        
-    def test_register_template(self):
-        response = self.client.get('/register')
-        self.assertTemplateUsed(response, 'home/register.html')
-        self.assertContains(response, 'Create account')
+        self.assertContains(response, "Back to home")
+
+class RegisterPageTemplateTests(TestCase):
+    def test_template_used(self):
+        response = self.client.get("/register")
+        self.assertTemplateUsed(response, "home/register.html")
+        self.assertContains(response, "Create account")
+    
+    def test_template_content(self):
+        response = self.client.get("/register")
+        self.assertContains(response, "Create account")
+        self.assertContains(response, "Username:")
+        self.assertContains(response, "Password:")
+        self.assertContains(response, "Password confirmation:")
+        self.assertContains(response, "Submit")
--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,1 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+class HomeTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username="user", password="password")
+    
+    def test_home_context_logged_out(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Log in")
+        self.assertNotContains(response, "Log out")
+    
+    def test_home_context_logged_in(self):
+        self.client.login(username="user", password="password")
+        response = self.client.get("/")
+        self.assertNotContains(response, "Log in")
+        self.assertContains(response, "Log out")
+        self.assertContains(response, f"Hey there, {self.user}!")
